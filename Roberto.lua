1c01a56256bcd9ac86f3ca389fa53ad811a13f9a
lX ROBERTO <gassanhassan474@gmail.com>
2025-02-23 11:41:09 +0300

    Update and rename LemoniumHub.lua to Roberto.lua

--- Roberto.lua
+++ Roberto.lua
@@ -3,7 +3,7 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Lemonium Hub 🍋 " .. Fluent.Version,
+    Title = "RIBERTO MAPS " .. Fluent.Version,
     SubTitle = "made by dausita helped by wallyxlv",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
@@ -44,8 +44,8 @@ local Options = Fluent.Options
 SaveManager:SetLibrary(Fluent)
 InterfaceManager:SetLibrary(Fluent)
 
-SaveManager:SetFolder("LemoniumHub")
-InterfaceManager:SetFolder("LemoniumHub")
+SaveManager:SetFolder("ROBERTO")
+InterfaceManager:SetFolder("ROBERTO")
 
 SaveManager:IgnoreThemeSettings()
 
